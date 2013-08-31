ce5c8d2f2ef7c7ddc5e533f15be3e6492c8620d0
Duncan Townsend <duncant@mit.edu>
2013-08-31 04:15:01 -0400

    Brought oaep.py in line with PKCS#1v2 (RFC3447)

--- oaep.py
+++ oaep.py
@@ -5,71 +5,116 @@ from itertools import *
 from keccak import Keccak
 from binascii import hexlify, unhexlify
 
-def oaep_keccak(k0, k1, m, random=random):
-    """Perform OAEP with Keccak as the one-way function
+def oaep_keccak(m, label='', out_len=None, hash_len=32, random=random, keccak_args=dict()):
+    """Perform OAEP (as specified by PKCS#1v2) with Keccak as the one-way function
 
-    k0: first OAEP parameter (in *bytes* not bits)
-    k1: second OAEP parameter (in *bytes* not bits)
+    All lengths specified in *bytes*
     m: message to be padded
-    random: entropy source for OAEP
+    label: (optional) to be associated with the message, the default is the empty string
+    out_len: (optional) the length of the message after padding, the default is len(m) + 2*hash_len + 2
+    hash_len: (optional) the length of the output of the hash algorithm, the default is 32
+    random: (optional) source of entropy for the random seed generation, the default is python's random module
+    keccak_args: (optional) parameters for the Keccak sponge function, the defaults are the Keccak defaults
     """
-    # pad m with zeroes
-    m = m+'\x00'*k1
+    if out_len is not None and len(m) > out_len - 2*hash_len - 2:
+        raise ValueError("Message too long to specified output and hash lengths")
+    
+    # hash the label
+    k = Keccak(**keccak_args)
+    k.soak(label)
+    lhash = k.squeeze(hash_len)
 
-    # generate r, a k0-byte random string
-    r = random.getrandbits(k0*8)
-    r = hex(int(r))[2:]
-    if r[-1] == 'L':
-        r = r[:-1]
-    r = '0'*(k0*2 - len(r)) + r
-    r = unhexlify(r)
+    if out_len is not None:
+        pad_string = '\x00' * (out_len - len(m) - 2*hash_len - 2)
+    else:
+        pad_string = ''
+    
+    # pad m
+    padded = lhash + pad_string + '\x01' + m
 
-    # expand r to the length of m
-    k = Keccak() # TODO: perhaps choose different parameters for G and H
-    k.soak(r)
-    G = k.squeeze(len(m))
+    # generate rand_seed, a hash_len-byte random string
+    rand_seed = random.getrandbits(hash_len*8)
+    rand_seed = hex(int(rand_seed))[2:]
+    if rand_seed[-1] == 'L':
+        rand_seed = rand_seed[:-1]
+    rand_seed = '0'*(hash_len*2 - len(rand_seed)) + rand_seed
+    rand_seed = unhexlify(rand_seed)
+
+    # expand rand_seed to the length of padded
+    k = Keccak(**keccak_args)
+    k.soak(rand_seed)
+    mask = k.squeeze(len(padded))
 
     # XOR the message with the expanded r
-    X = ''.join(imap(chr, imap(xor, imap(ord, m),
-                                    imap(ord, G))))
+    masked = ''.join(imap(chr, imap(xor, imap(ord, padded),
+                                         imap(ord, mask))))
 
-    # XOR r with the hash of the XOR'd message
-    k = Keccak() # TODO: perhaps choose different parameters for G and H
-    k.soak(X)
-    H = k.squeeze(len(r))
+    # hash masked to generate the seed mask
+    k = Keccak(**keccak_args)
+    k.soak(masked)
+    seed_mask = k.squeeze(len(rand_seed))
 
-    Y = ''.join(imap(chr, imap(xor, imap(ord, r),
-                                    imap(ord, H))))
+    # mask the seed
+    masked_seed = ''.join(imap(chr, imap(xor, imap(ord, rand_seed),
+                                              imap(ord, seed_mask))))
 
     # concatenate the two together
-    return X + Y
+    return '\x00' + masked_seed + masked
 
-def unoaep_keccak(k0, k1, XY):
-    """Recover a message padded with OAEP with Keccak as the one-way function
+def unoaep_keccak(m, label='', hash_len=32, keccak_args=dict()):
+    """Recover a message padded with OAEP (as specified by PKCS#1v2) with Keccak as the one-way function
 
-    k0: first OAEP parameter (in *bytes* not bits)
-    k1: second OAEP parameter (in *bytes* not bits)
-    XY: padded message to be recovered
+    All lengths specified in *bytes*
+    m: message to be decoded
+    label: (optional) the label expected on the message, the default is the empty string
+    hash_len: (optional) the length of the output of the hash algorithm, the default is 32
+    keccak_args: (optional) parameters for the Keccak sponge function, the defaults are the Keccak defaults
     """
-    # split the two parts of the OAEP'd message
-    X, Y = XY[:-k0], XY[-k0:]
+    # hash the label
+    k = Keccak(**keccak_args)
+    k.soak(label)
+    lhash = k.squeeze(hash_len)
 
-    # recover r
-    k = Keccak()
-    k.soak(X)
-    H = k.squeeze(len(Y))
+    # split the three parts of the OAEP'd message
+    Y, masked_seed, masked = m[0], m[1:hash_len+1], m[hash_len+1:]
 
-    r = ''.join(imap(chr, imap(xor, imap(ord, Y),
-                                    imap(ord, H))))
+    # recover rand_seed
+    k = Keccak(**keccak_args)
+    k.soak(masked)
+    seed_mask = k.squeeze(len(masked_seed))
 
-    # recover m
-    k = Keccak()
-    k.soak(r)
-    G = k.squeeze(len(X))
+    rand_seed = ''.join(imap(chr, imap(xor, imap(ord, masked_seed),
+                                            imap(ord, seed_mask))))
 
-    m = ''.join(imap(chr, imap(xor, imap(ord, X),
-                                    imap(ord, G))))
+    # recover the original message
+    k = Keccak(**keccak_args)
+    k.soak(rand_seed)
+    mask = k.squeeze(len(masked))
 
-    # trim the zero padding
-    assert sum(imap(ord, m[-k1:])) == 0
-    return m[:-k1]
+    padded = ''.join(imap(chr, imap(xor, imap(ord, masked),
+                                         imap(ord, mask))))
+
+    # find the index of the '\x01' separator byte without leaking timing info
+    found_separator = False
+    separator_index = hash_len
+    tru = True
+    for i in xrange(hash_len,len(padded)):
+        if (not found_separator) + (padded[i] != '\x00') == 2: # use + to avoid short-circuiting
+            separator_index = i
+            found_separator = tru
+        else:
+            separator_index = separator_index
+            found_separator = found_separator
+            
+    lhash_, pad_string, separator, retval = padded[:hash_len], padded[hash_len:separator_index], \
+                                            padded[separator_index], padded[separator_index+1:]
+
+    # check that lhash matches, the separator is correct, and the leading NUL is preserved
+    # without leaking which one failed
+    if sum([ sum(imap(xor, imap(ord, lhash),
+                           imap(ord, lhash_))) == 0,
+             separator == '\x01',
+             Y == '\x00' ]) != 3:
+        raise ValueError("Decryption failed")
+    else:
+        return retval
